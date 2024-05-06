505fea87a663ac7a4880c91b66f928b4ebf15406
hj022975 <67865647+hj022975@users.noreply.github.com>
2024-05-06 17:05:37 +0200

    Restore '6.5.2024, 16:58:27 Update main.ts'

--- main.ts
+++ main.ts
@@ -517,6 +517,7 @@ namespace hidotstar {
     //% blockId="spi_dotstar_send_buffer" blockHidden=true 
     //% shim=hidotstar::spiDotStarSendBuffer
     export function spiSendBuffer(buf: Buffer, len: number): void {
+        return; //## seems to be necessary
     }
 
     export enum HueInterpolationDirection {
