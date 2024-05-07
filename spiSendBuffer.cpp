5138cae3920f9c3cfbe160187caf5a7b642a8fd7
hj022975 <67865647+hj022975@users.noreply.github.com>
2024-05-07 17:08:36 +0200

    Update spiSendBuffer.cpp

--- spiSendBuffer.cpp
+++ spiSendBuffer.cpp
@@ -15,7 +15,7 @@ namespace hidotstar {
 	//%
 	void spiDotStarSendBuffer(Buffer buf, int len) {
 		SPI* spi = pins::allocSPI();
-	    spi->frequency(40000);
+	    spi->frequency(16000000);
 		// Send zero frame initially
 		for (int8_t i = 0; i < 4; i++) {
 			spi->write(0x00);
